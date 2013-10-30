7280e8f1f1339fe370e884ad67839157f300fa68
unknown <rsantos@SSI00017.intranet.chuv>
2013-10-30 16:15:45 +0100

    database option

--- softToDate.py
+++ softToDate.py
@@ -3,7 +3,9 @@
 from bs4 import BeautifulSoup as bs
 import urllib
 import re
-
+import argparse as ap
+import pyodbc
+import ConfigParser as cp
 
 class Software:
 	def __init__(self, name, version):
@@ -29,10 +31,64 @@ def parseTextFile(file):
 		softs.append(Software(appName.rstrip('\r\n'), '1.0'))
 	return softs
 
+def main():
+	# Arguments parsing
+	parser = ap.ArgumentParser(description='Software version checkker')
+	parser.add_argument('-f', '--file',
+		metavar='SOFTWARES_FILE',
+		default='softwares.txt',
+		help='path to text file containing software information')
+	parser.add_argument('--sql',
+		metavar='DB_CONFIG_FILE',
+		help='path to database config file')
+	args = parser.parse_args()
+	softwareFile = args.file
 
-if __name__ == '__main__':
-	softwares = parseTextFile('softwares.txt')
-	print len(softwares), 'softwares found'
+	# Database mode
+	if args.sql != None:
+		try:
+			config = cp.RawConfigParser(allow_no_value=True)
+			config.read('.mr.developer.cfg')
+			server = config.get('SQL', 'server')
+			database = config.get('SQL', 'database')
+			user = config.get('SQL', 'user')
+			pwd = config.get('SQL', 'pwd')
+			query = config.get('SQL', 'query')
+		except Exception as detail:
+			print "ERROR: configuration file format (", detail, ")"
+			exit(1)
+
+		try:
+			conStr = 'DRIVER={SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s' % (server, database, user, pwd)
+			con = pyodbc.connect(conStr)
+		except Exception as detail:
+			print "ERROR: can't connect to database (", detail, ")"
+			exit(1)
+
+		try:
+			cursor = con.cursor()
+			cursor.execute(query)
+			rows = cursor.fetchall()
+		except Exception as detail:
+			print "ERROR: query failed (", detail, ")"
+			exit(1)
+
+		softwares = []
+		for row in rows:
+			softwares.append(Software(row.nom, '1.0'))
+
+		print len(softwares), 'softwares found in database'
+
+	# Filetext mode
+	else:
+		softwares = parseTextFile(softwareFile)
+		print len(softwares), 'softwares found in file'
+
+	# WORK
 	for software in softwares:
 		software.getLatestVersion()
 		print software.name, '>', software.latestVersion
+
+
+if __name__ == '__main__':
+	app = main()
